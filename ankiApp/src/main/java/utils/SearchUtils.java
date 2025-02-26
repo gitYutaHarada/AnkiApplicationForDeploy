f2ab1e5189b86ae313e341b4cb4c159b6640b44d
haradayuuta <[a5822100@aoyama.jp]>
2025-02-26 14:43:29 +0900

    searchWord update

--- ankiApp/src/main/java/utils/SearchUtils.java
+++ ankiApp/src/main/java/utils/SearchUtils.java
@@ -3,19 +3,25 @@ package utils;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import bean.DataOfFile;
 
 public class SearchUtils {
 	
 	public List<Integer> searchWord(DataOfFile dataOfFile, String searchWord) {
-		HashMap<Integer, String> question = dataOfFile.getQuestionMap();
-		HashMap<Integer, String> answer = dataOfFile.getAnswerMap();
+		HashMap<Integer, String> questions = dataOfFile.getQuestionMap();
+		HashMap<Integer, String> answers = dataOfFile.getAnswerMap();
 		List<Integer> searchIds = new ArrayList<>();
 		
-		for(int id = 1 ; id <= dataOfFile.getMaxId(); id++ ) {
-			if(question.get(id).contains(searchWord) || answer.get(id).contains(searchWord)) {
-				searchIds.add(id);
+		for(Map.Entry<Integer, String> question : questions.entrySet()) {
+			if(question.getValue().contains(searchWord)) {
+				searchIds.add(question.getKey());
+			}
+		}
+		for(Map.Entry<Integer, String> answer : answers.entrySet()) {
+			if(answer.getValue().contains(searchWord) && !searchIds.contains(answer.getKey())) {
+				searchIds.add(answer.getKey());
 			}
 		}
 		return searchIds;
